7c92f1e47f2747968b2111de6ff3d82d953252b6
ssraible@gmail.com <ssraible@gmail.com>
2022-04-12 19:39:42 -0400

    voice recognition, volume threshold

--- software/bp_src/bp_src.ino
+++ software/bp_src/bp_src.ino
@@ -13,21 +13,26 @@
 #define OLED_DC A1
 #define OLED_RESET 13
 
+
+// Comment out if not using OLED
+//#define OLED
 Adafruit_SSD1305 display(128, 64, OLED_MOSI, OLED_CLK, OLED_DC, OLED_RESET, OLED_CS);
-///////////////////////////////////////////////
-// FINGERPRINT SENSOR
-///////////////////////////////////////////////
 
-#include <Adafruit_Fingerprint.h>
-#define mySerial Serial
-Adafruit_Fingerprint finger = Adafruit_Fingerprint(&mySerial);
-
+/////////////////////////////////////
 // Utilities: 
-
-// define print functions for use w/ screen
-#define DP_PRINTLN(x) display.println(F(x))   //use F(X) to save the string to Flash
-#define DP_PRINT(x) display.print(F(x))       //use F(X) to save the string to Flash
-
+#ifdef OLED
+#define DP_PRINTLN(x) { \
+  display.println(F(x)); \  //use F(X) to save the string to Flash
+  display.display()
+}
+#define DP_PRINT(x) { \
+  display.print(F(x)); \  //use F(X) to save the string to Flash
+  display.display()
+}
+#else
+#define DP_PRINTLN(x) Serial.println(F(x))   //use F(X) to save the string to Flash
+#define DP_PRINT(x) Serial.print(F(x))       //use F(X) to save the string to Flash
+#endif
 
 ///////////////////////////////////
 //        GAME SETTINGS          //
@@ -65,52 +70,47 @@ enum inputs {
 
 State states[] = {start_screen, handle_input, wait_and_select_input, game_over};
 
-inputs input = 0;
-int current_state = 0;
+inputs input = (inputs)0;
+int current_state = START_SCREEN;
 int next_state = 0;
 int score = 0;
 bool over = false;
 
 ////////////////////////////////////
+//          MICROPHONE            //
+////////////////////////////////////
+#define MIC_IN A5
+#define BG_SCALE 1.3
+
+bool get_voice_input();
+int background_sound = 0;
+
+////////////////////////////////////
 
 void reset_display(){
+  #ifdef OLED
   display.clearDisplay(); //use this to clear display
 
   //clear display resets all settings, configure text:
   display.setTextSize(1);
   display.setTextColor(WHITE);
   display.setCursor(0,0); //cursor start in top left
+  #endif
 }
 
 void setup() {
   Serial.begin(9600);
   while (! Serial) delay(100);
-  
+
+  #ifdef OLED
   if ( ! display.begin(0x3C) ) {
      while (1) yield();
   }
+  #endif
 
+  reset_display();
 
-  // set the data rate for the sensor serial port
-  finger.begin(57600);
-//  delay(5);
-  finger.LEDcontrol(FINGERPRINT_LED_FLASHING, 25, FINGERPRINT_LED_RED, 10);
-  display.clearDisplay();
-
-//  // set the data rate for the sensor serial port
-//  if (finger.verifyPassword()) {
-//    DP_PRINTLN("Found fingerprint sensor!");
-//    display.display();
-//    delay(300);
-//    display.clearDisplay();
-//  } else {
-//    DP_PRINTLN("Did not find fingerprint sensor :(");
-//    display.display();
-//    delay(300);
-//    display.clearDisplay();
-//    while (!finger.verifyPassword()) { delay(1); }
-//    
-//  }
+  background_sound = 1.30*analogRead(MIC_IN);
 
 }
 
@@ -124,14 +124,14 @@ bool check_start_input(){
   delay(10);
   return true;
 }
+
+
 void start_screen() {
-  display.setTextSize(1);
-  display.setTextColor(WHITE);
-  display.setCursor(0,0);
+  reset_display();
   
   DP_PRINTLN("Disarm-it!");
   DP_PRINTLN("Scan finger to start...");
-  display.display();
+
 
   while(!check_start_input()){}
 
@@ -156,8 +156,7 @@ void wait_and_select_input(){
         break;
     }
 
-    display.display();
-    next_state = handle_input();
+    next_state = HANDLE_INPUT;
   }
   else{
     next_state = GAME_OVER;
@@ -168,36 +167,37 @@ void wait_and_select_input(){
 
 void handle_input(){
   reset_display();
+  
   DP_PRINTLN("handling input");
-  display.display();
   delay(100);
   
   switch(input){
     case FINGERPRINT_SCANNER:
       //call fingerprint scanner fn
+      break;
     case KEYPAD:
       //call keypad function
+      break;
     case MICROPHONE:
-      //call microphone function
+      over = get_voice_input();
       break;
   }
+  next_state = WAIT_AND_SELECT_INPUT;
+}
+
+bool get_voice_input(){
+  DP_PRINTLN("Voice recognition");
+  DP_PRINTLN("Speak into the microphone");
+  bool g_over = true;
+  
+  while(g_over){
+    int current = analogRead(MIC_IN);
+    g_over = current<=background_sound;
+  }
+  
+  return g_over;
 }
 
 void game_over(){
   
 }
-
-
-//  DP_PRINTLN("This is how you print a new line");
-//  display.display();  //call this to flash to the display
-//  delay(300);         //delay 300ms (clocck no good)
-//  DP_PRINTLN("another line");
-//  display.display();
-//  delay(300);
-//
-//  display.clearDisplay(); //use this to clear display
-//
-//  //clear display resets all settings, configure text:
-//  display.setTextSize(1);
-//  display.setTextColor(WHITE);
-//  display.setCursor(0,0); //cursor start in top left
