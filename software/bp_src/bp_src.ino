67bdb435e826220b7af755eefd2f6bbe0e7f33d4
perihassanzadeh <70037334+perihassanzadeh@users.noreply.github.com>
2022-04-19 22:48:11 -0400

    gameover screen

--- software/bp_src/bp_src.ino
+++ software/bp_src/bp_src.ino
@@ -13,6 +13,32 @@
 #define OLED_DC A1
 #define OLED_RESET 13
 
+#define NUMFLAKES 10
+#define XPOS 0
+#define YPOS 1
+#define DELTAY 2
+
+#define LOGO16_GLCD_HEIGHT 16 
+#define LOGO16_GLCD_WIDTH  16 
+static const unsigned char PROGMEM logo16_glcd_bmp[] =
+{ B00000000, B11000000,
+  B00000001, B11000000,
+  B00000001, B11000000,
+  B00000011, B11100000,
+  B11110011, B11100000,
+  B11111110, B11111000,
+  B01111110, B11111111,
+  B00110011, B10011111,
+  B00011111, B11111100,
+  B00001101, B01110000,
+  B00011011, B10100000,
+  B00111111, B11100000,
+  B00111111, B11110000,
+  B01111100, B11110000,
+  B01110000, B01110000,
+  B00000000, B00110000 };
+
+void testdrawbitmap(const uint8_t *bitmap, uint8_t w, uint8_t h);
 
 // Comment out if not using OLED
 #define OLED
@@ -284,14 +310,17 @@ bool get_voice_input(){
 
 void game_over(){
   tone(TONE_PIN, 100, 1000);
-
-  
+  testdrawbitmap(logo16_glcd_bmp, LOGO16_GLCD_HEIGHT, LOGO16_GLCD_WIDTH);
   DP_PRINTLN("GAME OVER!");
   DP_PRINT("YOUR SCORE IS: ");
   display.print(score);
   DISPLAY();
 
+  score = 0;
+  over = false;
+
   delay(10000);
+  current_touchstate = digitalRead(touchsensor);
   next_state = START_SCREEN;
 }
 
@@ -354,4 +383,46 @@ bool fingerprint_scanner_input(){
   }
 
   return true;
+}
+
+void testdrawbitmap(const uint8_t *bitmap, uint8_t w, uint8_t h) {
+  uint8_t icons[NUMFLAKES][3];
+  randomSeed(666);     // whatever seed
+ 
+  // initialize
+  for (uint8_t f=0; f< NUMFLAKES; f++) {
+    icons[f][XPOS] = random(display.width());
+    icons[f][YPOS] = 0;
+    icons[f][DELTAY] = random(5) + 1;
+    
+    Serial.print("x: ");
+    Serial.print(icons[f][XPOS], DEC);
+    Serial.print(" y: ");
+    Serial.print(icons[f][YPOS], DEC);
+    Serial.print(" dy: ");
+    Serial.println(icons[f][DELTAY], DEC);
+  }
+  int count = 0;
+  while (count<13) {
+    // draw each icon
+    for (uint8_t f=0; f< NUMFLAKES; f++) {
+      display.drawBitmap(icons[f][XPOS], icons[f][YPOS], logo16_glcd_bmp, w, h, WHITE);
+    }
+    display.display();
+    delay(200);
+    
+    // then erase it + move it
+    for (uint8_t f=0; f< NUMFLAKES; f++) {
+      display.drawBitmap(icons[f][XPOS], icons[f][YPOS],  logo16_glcd_bmp, w, h, BLACK);
+      // move it
+      icons[f][YPOS] += icons[f][DELTAY];
+      // if its gone, reinit
+      if (icons[f][YPOS] > display.height()) {
+        icons[f][XPOS] = random(display.width());
+        icons[f][YPOS] = 0;
+        icons[f][DELTAY] = random(5) + 1;
+      }
+    }
+    count++;
+   }
 }
